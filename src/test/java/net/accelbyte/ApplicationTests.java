5f14f4715804b154b96e7ae8e1561afbd4f7b5ce
Eric Chan <eric.chan@accelbyte.net>
2024-03-03 00:24:30 -0800

    fix: add missing mocked config like base url, etc

--- src/test/java/net/accelbyte/ApplicationTests.java
+++ src/test/java/net/accelbyte/ApplicationTests.java
@@ -1,10 +1,14 @@
 package net.accelbyte;
 
+import net.accelbyte.config.MockedAppConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+		classes = MockedAppConfiguration.class,
+		properties = "spring.main.allow-bean-definition-overriding=true"
+)
 @ActiveProfiles("test")
 class ApplicationTests {
 
---
+++ src/test/java/net/accelbyte/config/MockedAppConfiguration.java
@@ -0,0 +1,17 @@
+package net.accelbyte.config;
+
+import net.accelbyte.sdk.core.AccelByteSDK;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+@TestConfiguration
+public class MockedAppConfiguration {
+
+    @Bean
+    @Primary
+    public AccelByteSDK accelbyteSdk() {
+        return Mockito.mock(AccelByteSDK.class);
+    }
+}
